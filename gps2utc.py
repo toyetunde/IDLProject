2c904dffa336f7a1d286f84c0552c31bff5dcf67
toyetunde <40206510+toyetunde@users.noreply.github.com>
2023-08-05 22:47:34 -0300

    GPS time to UTC time
    This function converts the GPS time to universal time.

---
+++ gps2utc.py
@@ -0,0 +1,21 @@
+from astropy.time import Time
+def gps2utc(gpsweek, gpsseconds):
+    """ GPS time to UTC.
+    Parameters
+    ----------
+    gpsweek : int
+        GPS week number, i.e. 1866.
+    gpsseconds : int
+        Number of seconds since the beginning of week.
+    Returns
+    -------
+    datetime
+        datetime instance with UTC time.
+    """
+    secs_in_week = 604800
+    secs = gpsweek * secs_in_week + gpsseconds
+
+    t_gps = Time(secs, format="gps")
+    t_utc = Time(t_gps, format="iso", scale="utc")
+
+    return t_utc.datetime
